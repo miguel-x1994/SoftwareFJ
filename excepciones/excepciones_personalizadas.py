28abd4b0ce375a947f270ef4dbafd74570367d73
miguel-x1994 <mejimenez94@gmail.com>
2026-05-10 23:17:31 -0500

    Update excepciones_personalizadas.py

--- excepciones/excepciones_personalizadas.py
+++ excepciones/excepciones_personalizadas.py
@@ -6,22 +6,35 @@ utilizadas en el sistema Software FJ para el manejo de errores.
 
 Fecha: 2026
 """
-
+# Excepcion base del sistema
+class SofwareFJError(Exception):
+    def __init__(self, mensaje,codigo="Error-000"):
+        super().__init__(mensaje)
+        self.mensaje = mensaje
+        self.codigo = codigo
+        
+    def __str__(self):
+        return f"[{self.codigo}] {self.mensaje}"
+    
 # Excepcion para errores en datos del cliente
-class ClienteInvalidoError(Exception):
-    pass
+class ClienteInvalidoError(SofwareFJError):
+    def __init__(self, mensaje):
+        super().__init__(mensaje, "Error-101")
 
 
 # Excepcion para servicios no disponibles o incorrectos
-class ServicioNoDisponibleError(Exception):
-    pass
+class ServicioNoDisponibleError(SofwareFJError):
+    def __init__(self, mensaje):
+        super().__init__(mensaje, "Error-201")
 
 
 # Excepcion general para errores en reservas
-class ReservaError(Exception):
-    pass
+class ReservaError(SofwareFJError):
+    def __init__(self, mensaje):
+        super().__init__(mensaje, "Error-301")
 
 
 # Excepcion para duracion invalida en reservas
-class DuracionInvalidaError(Exception):
-    pass
+class DuracionInvalidaError(SofwareFJError):
+    def __init__(self, mensaje):
+        super().__init__(mensaje, "Error-302")
