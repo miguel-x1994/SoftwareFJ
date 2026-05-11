cd96fdfa1d2c09a234d7140c53dae3c55af5e1f3
miguel-x1994 <mejimenez94@gmail.com>
2026-05-10 23:35:43 -0500

    Update entidad.py

--- modelos/entidad.py
+++ modelos/entidad.py
@@ -13,7 +13,16 @@ from abc import ABC, abstractmethod
 # Clase abstracta base para todas las entidades del sistema
 class Entidad(ABC):
 
-    # Metodo abstracto que debe ser implementado por las clases hijas
+    # Metodo abstracto obligatoro
     @abstractmethod
     def mostrar_info(self):
         pass
+    
+    #Metodo abstracto obligario para validar datos
+    @abstractmethod
+    def validar(self):
+        pass
+    
+    #Metodo especial para mostar informa automatica
+    def __str__(self):
+        return self.mostrar_info()
