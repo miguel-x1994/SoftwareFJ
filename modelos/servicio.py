e39a79b5d4ca36fcc4e96380323cdf150b05ef84
miguel-x1994 <mejimenez94@gmail.com>
2026-05-11 08:34:23 -0500

    Update servicio.py

--- modelos/servicio.py
+++ modelos/servicio.py
@@ -10,24 +10,72 @@ Fecha: 2026
 """
 
 from abc import ABC, abstractmethod
-
+from excepciones.excepciones_personalizadas import (
+    ServicioNoDisponibleError
+)
 
 # Clase abstracta base para todos los servicios del sistema
 class Servicio(ABC):
 
     # Constructor de la clase
     def __init__(self, nombre, tarifa_base):
+        
+        #validacion de tarifa
+        if tarifa_base <= 0:
+            raise ServicioNoDisponibleError(
+                "ka tarifa base debe ser mayor a 0"
+            )
 
         # Atributos del servicio
-        self.nombre = nombre
+        self.nombre = nombre.strip()
         self.tarifa_base = tarifa_base
+        self.disponible = True
+    
+    # Metod para verificar disponibilidad
+    def verificar_disponibilidad(self):
+        
+        if not self.disponible:
+            raise ServicioNoDisponibleError(
+                f"El servicio {self.nombre} no esta disponible"
+            )
 
     # Metodo abstracto para calcular el costo del servicio
     @abstractmethod
-    def calcular_costo(self):
+    def calcular_costo(self, duracion):
         pass
 
     # Metodo abstracto para describir el servicio
     @abstractmethod
     def descripcion(self):
         pass
+    
+    # Metodo para mostar informacion
+    def mostar_info(self):
+        
+        estado = "Disponible" if self.disponible else "No disponible"
+        
+        return f"""
+    
+    Servicio: {self.nombre}
+    Tarifa base: ${self.tarifa_base}
+    Estado: {estado}
+    """
+    
+        # Metodo para validar datos
+        def validar(self):
+            
+            return (
+                self.nombre != ""
+                and self.tarifa_base > 0
+            )
+            
+        # Metodo especial str
+        def __str__(self):
+            
+            return self.mostar_info()
+        
+        def get_nombre(self):
+            return self.nombre
+            
+        def get_tarifa_base(self):
+            return self.tarifa_base
